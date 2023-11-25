31605c4ca6d82473ff5cb7a283ac20ec4ce4001b
cureseven <shiningcureseven@gmail.com>
2023-11-25 15:16:06 +0900

    fix:ownerの処理は戻した

--- webapp/go/livestream_handler.go
+++ webapp/go/livestream_handler.go
@@ -485,6 +485,12 @@ func getLivecommentReportsHandler(c echo.Context) error {
 }
 
 func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel LivestreamModel) (Livestream, error) {
+	ownerModel := UserModel{}
+	if err := tx.GetContext(ctx, &ownerModel, "SELECT * FROM users WHERE id = ?", livestreamModel.UserID); err != nil {
+		return Livestream{}, err
+	}
+	owner, err := fillUserResponse(ctx, tx, ownerModel)
+
 	type livestreamTagResult struct {
 		LivestreamModel
 		TagID   sql.NullInt64  `db:"tag_id"`
@@ -500,7 +506,7 @@ LEFT JOIN livestream_tags lt ON lt.livestream_id = l.id
 LEFT JOIN tags t ON t.id = lt.tag_id
 WHERE l.id = ?
 `
-	err := tx.SelectContext(ctx, &results, query, livestreamModel.ID)
+	err = tx.SelectContext(ctx, &results, query, livestreamModel.ID)
 	if err != nil {
 		return Livestream{}, err
 	}
@@ -518,11 +524,6 @@ WHERE l.id = ?
 				Name: result.TagName.String,
 			})
 		}
-	}
-
-	owner, err := fillUserResponse(ctx, tx, UserModel{ID: livestreamData.UserID})
-	if err != nil {
-		return Livestream{}, err
 	}
 
 	livestream := Livestream{

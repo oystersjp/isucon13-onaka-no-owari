bcc0a946753cecb9e93e53e9eeca1f9d713f791f
cureseven <shiningcureseven@gmail.com>
2023-11-25 11:08:43 +0900

    fix:v4消してしまっていた

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/gorilla/sessions"
